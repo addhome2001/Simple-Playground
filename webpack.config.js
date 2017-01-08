f871fb1dd2c56164305eea415c06b5da2239843f
Seven <seven@megalive.com.tw>
2017-01-08 13:32:50 +0800

    Added eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,58 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
     bundle: [
       'webpack-dev-server/client?http://localhost:8000',
       'webpack/hot/dev-server',
-      './src/index.js'
-    ]
+      './src/index.js',
+    ],
   },
   devServer: {
-     host: 'localhost',
-     port: 8000,
-     contentBase: './dest',
+    host: 'localhost',
+    port: 8000,
+    contentBase: './dest',
   },
-	output: {
+  output: {
     path: path.join(__dirname, '/dest'),
-		filename: 'bundle.js',
+    filename: 'bundle.js',
     /* If you want to expose 'ClassName' to global namespace => window.ClassName
       library: 'ClassName',
     */
-	},
+  },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin(
       {
         'process.env.NODE_ENV': JSON.stringify('development'),
-      }
+      },
     ),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
-      debug: false
+      debug: false,
     }),
   ],
   resolveLoader: {
-   	modulesDirectories: ['node_modules']
- 	},
+    modulesDirectories: ['node_modules'],
+  },
   resolve: {
-	  extensions: ['', '.js', 'jsx'],
+    extensions: ['', '.js', 'jsx'],
     modules: [
       path.resolve('./src'),
-      'node_modules'
-    ]
+      'node_modules',
+    ],
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         use: [{
-          loader: "babel",
-          options: { presets: ["latest"] },
-          query: { cacheDirectory: true }
+          loader: 'babel',
+          options: { presets: ['latest'] },
+          query: { cacheDirectory: true },
         }],
       },
-    ]
-  }
+    ],
+  },
 };
--- webpack.prov.config.js
+++ webpack.prov.config.js
@@ -3,34 +3,34 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: {
-      "bundle": './src/index.js'
+    bundle: './src/index.js',
   },
   output: {
-      path: path.join(__dirname, 'dest'),
-		  filename: '[name].js',
-	},
+    path: path.join(__dirname, 'dest'),
+    filename: '[name].js',
+  },
   plugins: [
-      new webpack.NoErrorsPlugin(),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
-        }
-      })
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
   ],
   resolve: {
-		extensions: ['', '.js', 'jsx'],
-    modules: [path.resolve(__dirname, "src"), "node_modules"]
-	},
-	module: {
+    extensions: ['', '.js', 'jsx'],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+  module: {
     rules: [
       {
         test: /\.js$/,
         use: [{
-          loader: "babel",
-          options: { presets: ["latest"] },
-          query: { cacheDirectory: true }
+          loader: 'babel',
+          options: { presets: ['latest'] },
+          query: { cacheDirectory: true },
         }],
       },
-    ]
-  }
+    ],
+  },
 };
