369621392bc8b5d5316e6b65a5ec3743b37ac4d1
Seven <addhome2001@yahoo.com.tw>
2018-12-01 11:31:02 +0800

    upgrade webpack to 4

--- config/dev.js
+++ config/dev.js
@@ -6,70 +6,67 @@ const WebpackNotifierPlugin = require('webpack-notifier');
 const HOST = process.env.HOST || 'localhost';
 const PORT = +process.env.PORT || 8000;
 
-module.exports = () =>
-  ({
-    entry: {
-      bundle: [
-        'webpack/hot/dev-server',
-        path.join(__dirname, '../src'),
-      ],
-    },
-    devtool: 'eval',
-    devServer: {
-      host: HOST,
-      port: PORT,
-      contentBase: path.join(__dirname, '../dist'),
-      historyApiFallback: true,
-    },
-    output: {
-      path: path.join(__dirname, '../dist'),
-      filename: '[name].js',
-      /* If you want to expose 'ClassName' to global namespace => window.ClassName
+module.exports = () => ({
+  entry: {
+    bundle: ['webpack/hot/dev-server', path.join(__dirname, '../src')],
+  },
+  devtool: 'eval',
+  mode: 'development',
+  devServer: {
+    host: HOST,
+    port: PORT,
+    contentBase: path.join(__dirname, '../dist'),
+    historyApiFallback: true,
+  },
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: '[name].js',
+    /* If you want to expose 'ClassName' to global namespace => window.ClassName
         library: 'ClassName',
       */
-    },
-    plugins: [
-      new WebpackNotifierPlugin(),
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.LoaderOptionsPlugin({
-        minimize: false,
-        debug: true,
-      }),
-      new webpack.ProvidePlugin({
-        Promise: 'es6-promise',
-        fetch: 'isomorphic-fetch',
-      }),
-      new HtmlWebpackPlugin({
-        title: 'Example',
-        filename: 'index.html',
-        template: path.join(__dirname, '../templates', 'index.ejs'),
-      }),
-      new webpack.EnvironmentPlugin({
-        NODE_ENV: 'development',
-        DEBUG: true,
-      }),
-    ],
-    resolve: {
-      extensions: ['.js'],
-      // 可以直接引入assets下的資源，但是需要加上assets前綴
-      // assets: path.resolve(__dirname, './src/assets'),
-      // 增加src可讓路徑根目錄為src
-      modules: ['node_modules'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules)/,
-          use: [
-            {
-              loader: 'babel-loader',
-              options: {
-                cacheDirectory: true,
-              },
+  },
+  plugins: [
+    new WebpackNotifierPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: false,
+      debug: true,
+    }),
+    new webpack.ProvidePlugin({
+      Promise: 'es6-promise',
+      fetch: 'isomorphic-fetch',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Example',
+      filename: 'index.html',
+      template: path.join(__dirname, '../templates', 'index.ejs'),
+    }),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development',
+      DEBUG: true,
+    }),
+  ],
+  resolve: {
+    extensions: ['.js'],
+    // 可以直接引入assets下的資源，但是需要加上assets前綴
+    // assets: path.resolve(__dirname, './src/assets'),
+    // 增加src可讓路徑根目錄為src
+    modules: ['node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
             },
-          ],
-        },
-      ],
-    },
-  });
+          },
+        ],
+      },
+    ],
+  },
+});
